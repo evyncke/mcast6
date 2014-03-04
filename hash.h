d26fbad1982bd8337a750c7540910a56d6db3331
Eric Vyncke <eric@vyncke.org>
2014-03-04 18:26:26 +0000

    Adding header file

---
+++ hash.h
@@ -0,0 +1,33 @@
+/*
+
+Trivial hash tables by evyncke@cisco.com
+
+January 18, 2014
+
+***********/
+
+
+struct hentry {
+	unsigned long int count ;
+	struct hentry * next ;
+	unsigned char * key ;
+} ;
+
+typedef struct {
+	unsigned long entries_count ;
+	unsigned long keys_count ;
+	unsigned int key_size ;
+	struct hentry * first ;
+} htable ;
+
+typedef void (*htable_printer)(unsigned char * key, unsigned int key_size) ;
+
+htable * htable_init(const unsigned int key_size) ;
+
+void htable_add(htable * table, void * key) ;
+
+void htable_dump(htable * table, htable_printer printer) ;
+
+unsigned long int htable_size(htable * table) ;
+
+void htable_ipv6_printer(unsigned char * key, unsigned int key_size) ;
